d472cc4e6b2f2722cef83644e36e2616de34b05b
Jonathan Experton <jonathan.experton@gmail.com>
2016-09-13 19:40:56 -0400

    elixir/bob edit after hickscorp comment

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -7,7 +7,7 @@ defmodule Bob do
   def answer(input) do
     cond do
         String.match?(input, ~r/^\s*$/) -> "Fine. Be that way!"
-        String.ends_with?(input, "?")   -> "Sure."
+        String.last(input) == "?" -> "Sure."
         !String.match?(input, ~r/\D/)   -> "Whatever."
         !String.match?(input, ~r/[^\p{Lu}\d]/) -> "Whoa, chill out!"
         true -> "Whatever."
