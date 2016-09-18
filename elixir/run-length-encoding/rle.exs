0f1ccda04152a1c6748392185f7862a599a75fab
Jonathan Experton <jonathan.experton@gmail.com>
2016-09-18 12:03:53 -0400

    elixir/run-length-encoding done without recursion

--- elixir/run-length-encoding/rle.exs
+++ elixir/run-length-encoding/rle.exs
@@ -7,27 +7,16 @@ defmodule RunLengthEncoder do
   "1H1O1R1S1E" => "HORSE"
   """
   @spec encode(String.t) :: String.t
-  def encode(""), do: ""
   def encode(string) do
-    String.graphemes(string) |> encodeR("")
-  end
-
-  @spec encodeR(List.t, String.t) :: String.t
-  def encodeR([], acc), do:  acc
-  def encodeR(graphemes, acc) do
-    [same|rest] = Enum.split_while(graphemes, &(&1 == Enum.at(graphemes, 0))) |> Tuple.to_list
-    encodeR(List.first(rest), acc <> "#{Enum.count(same)}#{List.first(same)}")
+    String.graphemes(string)
+    |> Enum.chunk_by(&(&1))
+    |> Enum.map_join(&("#{length(&1)}#{hd(&1)}"))
   end
 
   @spec decode(String.t) :: String.t
   def decode(string) do
-    String.split(string, ~r/\d+\D/, include_captures: true, trim: true)
-    |> Enum.map_join("", &(decodeR(&1)))
-  end
-
-  @spec decodeR(String.t) :: String.t
-  def decodeR(string) do
-    [h|t] = String.split(string, ~r/\D/, include_captures: true, trim: true)
-    List.duplicate(t, String.to_integer(h)) |> Enum.join
+    Regex.scan(~r/(\d+)(\D)/, string)
+    |> Enum.map_join(fn [_, count, grapheme]
+      -> String.duplicate(grapheme, String.to_integer(count)) end)
   end
 end
