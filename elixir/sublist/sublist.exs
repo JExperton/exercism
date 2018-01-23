e93165f429dd8eec460f92e01537478ed592282f
Jonathan Experton <jonathan.experton@gmail.com>
2018-01-22 23:09:42 -0500

    elixir/sublist done

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -4,6 +4,13 @@ defmodule Sublist do
   and if not whether it is equal or unequal to the second list.
   """
   def compare(a, b) do
-
+    case {a, b} do
+      {a, a} -> :equal
+      {[], _} -> :sublist
+      {_, []} -> :superlist
+      {a, b} when length(a) == length(b) -> if a === b, do: :equal, else: :unequal
+      {a, b} when length(a) > length(b) -> if compare(b, a) == :sublist, do: :superlist, else: :unequal
+      _ -> if Enum.any?(0..(length(b) - length(a)), &(compare(Enum.slice(b, &1, length(a)), a) == :equal)), do: :sublist, else: :unequal
+    end
   end
 end
